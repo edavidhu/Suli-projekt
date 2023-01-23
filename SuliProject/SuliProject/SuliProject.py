dc29cc100e9058affb4e21b42cffce9118de4fd1
edavidhu <83879246+edavidhu@users.noreply.github.com>
2023-01-23 14:36:31 +0100

    Add files via upload

---
+++ SuliProject/SuliProject/SuliProject.py
@@ -0,0 +1,9 @@
+a = float(input("Adj meg egy egesz szamot: "))
+b = float(input("Adj meg egy masik egesz szamot: "))
+
+if a > b:
+    print("A nagyobb szam", a)
+elif a < b:
+    print("A nagyobb szam", b)
+elif a == b:
+    print("A ket szam egyenlo")
